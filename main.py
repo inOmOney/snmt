0657be307cb1f86e995912cb55366c8206ffc05e
Vcvc <vcvckw@gmail.com>
2020-12-23 21:16:11 +0800

    init

---
+++ main.py
@@ -0,0 +1,20 @@
+import time
+import re
+import os
+from src.Control import Control
+con = Control()
+
+if __name__ == '__main__':
+    a = """ 
+苏宁抢购-功能列表：                                                                                
+ 1. 预约商品
+ 2. 秒杀抢购商品
+"""
+    print(a)
+    num = input("请输入选项: ")
+
+    if num == '1' or num == '2':
+        con.main(num)
+    else:
+        print('退出')
+
