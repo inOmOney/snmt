c0e1d96a8c689ed775d8e94fb4ce8f99ba9ce715
Vcvc <vcvckw@gmail.com>
2020-12-24 21:16:52 +0800

    sell

--- src/sn_sell.py
+++ src/sn_sell.py
@@ -1,7 +1,13 @@
 import requests
 import json
-
+#from src.timer import timer
 
 class Sn_Sell(object):
     def __init__(self):
         pass
+
+    def main(self):
+        print("sell")
+
+sn_sell = Sn_Sell()
+
